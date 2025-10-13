6ea58ec4fc86eb486c7908bdbf36e0b75df51d7a
congdt22-beep <congdt22@uef.edu.vn>
2025-10-13 21:15:03 +0700

    Test render without router

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
-import Admin from "./Admin";
-import Join from "./Join";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(
   <React.StrictMode>
-    <Router basename="/">
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route
-          path="/join"
-          element={<Join onJoin={(name) => (window.location.href = "/")} />}
-        />
-        {/* fallback route nếu path không hợp lệ */}
-        <Route path="*" element={<App />} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 );
